8ef5ff551b37144246900a38b8572d44857ac720
Eduardo Costa <m4c0@users.noreply.github.com>
2023-01-10 16:47:42 +0000

    fix build order

--- build.cpp
+++ build.cpp
@@ -6,9 +6,9 @@ int main(int argc, char **argv) {
   auto all = unit::create<seq>("all");
 
   auto gzt = all->add_unit<exe>("gzt");
-  gzt->add_unit<>("gzt");
-  gzt->add_ref(zipline());
   gzt->add_wsdep("yoyo", yoyo());
+  gzt->add_ref(zipline());
+  gzt->add_unit<>("gzt");
 
   return ecow::run_main(gzt, argc, argv);
 }
