39d793b3d428d3715f056c128e00f2a20d1f8851
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-15 16:40:55 +0100

    poc for listing arbitrary ZIP files

---
+++ poc.cpp
@@ -0,0 +1,24 @@
+#pragma leco tool
+
+import jute;
+import silog;
+import yoyo;
+import zipline;
+
+static auto print(const zipline::cdir_entry &cdir) {
+  silog::log(silog::info, "%s", cdir.filename.begin());
+  return mno::req{};
+}
+
+static void list_zip(const char *name) {
+  yoyo::file_reader::open(name)
+      .fmap([](auto &r) { return zipline::list(r, print); })
+      .trace(jute::heap{"listing entries of "} + jute::view::unsafe(name))
+      .log_error();
+}
+
+int main(int argc, char **argv) {
+  for (auto i = 1; i < argc; i++) {
+    list_zip(argv[i]);
+  }
+}
